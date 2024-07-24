63ed38c33bcd3b2e951627223d7b41bce83378e4
Adrian Womack <awomack@lcls-dev3.slac.stanford.edu>
2024-07-24 14:55:48 -0700

    Resized main screen's window

--- mc_motor_calibration_main.py
+++ mc_motor_calibration_main.py
@@ -90,7 +90,7 @@ class MainWindow(Display):
         super(MainWindow, self).__init__(parent=parent, args=args, macros=macros)
 
         # Set initial size of main window
-        self.resize (600, 800)
+        self.resize (800, 800)
 
         # [EDIT FOR YOUR PERSONAL CONFIGURATIONS] Global Variable that holds the directory where data is to be saved
         self.path = os.environ.get("PHYSICS_DATA") + "/genMotion/lvdtCal" 
